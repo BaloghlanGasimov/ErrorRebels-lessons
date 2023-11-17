010e1d2e74b54a7aa00fc49e6f9863e3dfaf2b52
Baloghlan <qasimovbaloglan13@gmail.com>
2023-11-17 13:24:57 +0400

    backgroundImageProperties

---
+++ Lesson3-backgrpundImage/script.js
@@ -0,0 +1,18 @@
+// var,const,let
+
+// let number =3;
+// var reqem=4;
+
+
+// if(number>4){
+
+
+
+
+//     console.log("Number 2 den boyukdur");
+// }else{
+//     console.log("Number 2 den boyuk deyil");
+// }
+
+
+
