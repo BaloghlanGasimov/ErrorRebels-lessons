29f7eae72f51444317ad18109f4c2a4e0bee8e09
Baloghlan <qasimovbaloglan13@gmail.com>
2023-11-24 09:19:44 +0400

    starting javascript

---
+++ Lesson4-javascriptWhileAndIf/script.js
@@ -0,0 +1,46 @@
+// let number=3;
+
+// let b=2;
+    
+
+// while(b<5){
+
+
+//     // sum=sum+b;
+//     if(b==3){
+//         b++
+//         continue;
+//     }
+    
+//     console.log(b,"salam");
+//     b++;
+    
+
+
+// }
+
+// console.log("bitdi");
+
+
+let a=2;
+
+var sum=0;
+while(a<15){
+
+    sum=sum+a;
+    a++;
+
+    
+    console.log(sum);
+    
+}
+
+
+
+
+
+
+
+
+
+
