d245ae98e9f0b3c62910b25e69ff01063bc7ea0f
Baloghlan <qasimovbaloglan13@gmail.com>
2023-12-01 09:31:28 +0400

    some exercises

---
+++ lesson5-javascriptExercise/script.js
@@ -0,0 +1,51 @@
+// let s = "    salam        necesen    b               ";
+
+
+// let s = "necesenaa"
+            // s[9-1]
+// console.log(s[8]);
+
+// for(let i =0; i<7;){
+//     if(i==2){
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+
+// }
+
+
+
+
+
+// let trimS = s.trim();
+
+
+// let myString ="";
+
+
+// if(a==" ")
+
+// console.log(myString);
+
+
+// console.log(trimS);
+
+
+
+
+
+
+let s1 = "Salam   necesen";
+let newS = "";
+
+for(let i=0;i<s1.length;i++){
+
+    if(s1[i]!=" "){
+        newS+=s1[i];
+    }
+
+}
+
+console.log("Cavabimiz: ",newS);
