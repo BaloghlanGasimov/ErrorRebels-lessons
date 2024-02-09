fe13c313d9367ca4cc909f9f5ee7b21f5fe5558d
Baloghlan <qasimovbaloglan13@gmail.com>
2024-02-09 14:12:55 +0400

    detail page

--- Lesson14-DetailPageUseParams/src/ShopDetailPage.jsx
+++ Lesson14-DetailPageUseParams/src/ShopDetailPage.jsx
@@ -8,19 +8,29 @@ const ShopDetailPage = () => {
 
     const [apiData,setApiData] = useState([]);
 
+    const [loading,setLoading] = useState(true);
+
+    const [networkError,setNetworkError] = useState(false);
+
+
     console.log(key);
 
     useEffect(()=>{
         axios
-        .get(`https://fakestoreapi.com/products`)
+        .get(`https://fakestoreapi.com/products11`)
         .then((res)=>{
 
             setApiData(res.data)
+            setLoading(false)
 
+        })
+        .catch((err)=>{
+          setLoading(false);
+          setNetworkError(true);
         })
     },[])
 
-    const filteredProduct = apiData.filter(p=>p.id==key);
+    const filteredProduct = apiData.find(p=>p.id==key);
 
     console.log(filteredProduct);
 
@@ -29,7 +39,20 @@ const ShopDetailPage = () => {
 
         <p>id: {key}</p>
 
-        {filteredProduct.length==0? <p> loading...</p> : <p>{filteredProduct[0].title}</p> }
+        {/* {filteredProduct.length==0? <p> loading...</p> : <p>{filteredProduct[0].title}</p> } */}
+
+        {loading ?
+         <p>Loading...</p>
+         :
+
+         networkError
+         ?
+         <p>yuklenen zaman xeta oldu</p>
+         :
+         <img src={filteredProduct.image} alt="" />
+
+        
+        }
 
        
 
