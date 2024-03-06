748261a4541c98b69c14941b9fcf0849b22668a7
Baloghlan <qasimovbaloglan13@gmail.com>
2024-03-06 11:56:12 +0400

    new lesson

---
+++ Lesson16QuestionAnswer/script.js
@@ -0,0 +1,13 @@
+
+
+console.log("hi");
+
+// const div = document.querySelector(".test");
+
+// div.innerHTML ="<p>hello</p>"
+
+// div.textContent ="   s   s"
+
+// div.innerText ="    s   s "
+
+// div.innerText
